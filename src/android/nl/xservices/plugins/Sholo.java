df2e099969d13da9ec88f6640b3aa95de0703c87
Xavier Perez <xap5xap@gmail.com>
2017-01-08 10:57:10 -0500

    First

---
+++ src/android/nl/xservices/plugins/Sholo.java
@@ -0,0 +1,29 @@
+package nl.xservices.plugins;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Sholo extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if (action.equals("coolMethod2")) {
+          String message = args.getString(0);
+          this.coolMethod2(message, callbackContext);
+          return true;
+      }
+      return false;
+  }
+
+  private void coolMethod2(String message, CallbackContext callbackContext) {
+      if (message != null && message.length() > 0) {
+          callbackContext.success(message);
+      } else {
+          callbackContext.error("Expected one non-empty string argument.");
+      }
+  }
+}
---
+++ www/sholo.js
@@ -0,0 +1,17 @@
+function Sholo() {
+}
+
+Sholo.prototype.coolMethod3 = function(arg0, success, error) {
+  cordova.exec(success, error, "Sholo", "coolMethod2", [arg0]);
+};
+
+Sholo.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.sholo = new Sholo();
+  return window.plugins.sholo;
+};
+
+cordova.addConstructor(Sholo.install);ß
