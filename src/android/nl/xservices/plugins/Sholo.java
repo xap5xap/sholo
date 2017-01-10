a0399e127b754aeea84cea6e1f36d39dfdce0f6b
Xavier Perez <xap5xap@gmail.com>
2017-01-10 12:07:46 -0500

    fix call2

--- src/android/nl/xservices/plugins/Sholo.java
+++ src/android/nl/xservices/plugins/Sholo.java
@@ -1,27 +1,46 @@
 package nl.xservices.plugins;
 
-import org.apache.cordova.CordovaPlugin;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AlignmentSpan;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import org.apache.cordova.CallbackContext;
-import android.util.Log;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.apache.cordova.PluginResult;
+
 
 public class Sholo extends CordovaPlugin {
 
   @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     Log.d("xap","xap en execute");
       if (action.equals("coolMethod2")) {
         Log.d("xap","dentro de equal");
-          String message = args.getString(0);
-          this.coolMethod2(message, callbackContext);
-          Log.d("xap","retorna true");
+          // String message = args.getString(0);
+          // this.coolMethod2(message, callbackContext);
+          this.hide();
+          callbackContext.success();
           return true;
       }
       return false;
   }
+
+  private void hide() {
+    Log.d("xap","dentro de hide");
+}
 
   private void coolMethod2(String message, CallbackContext callbackContext) {
 
@@ -31,7 +50,7 @@ public class Sholo extends CordovaPlugin {
     PluginResult pr = new PluginResult(PluginResult.Status.OK);
     pr.setKeepCallback(true);
     callbackContext.sendPluginResult(pr);
-    
+
       // if (message != null && message.length() > 0) {
       //   Log.d("xap","va asuccess");
       //     callbackContext.success(message);
--- www/sholo.js
+++ www/sholo.js
@@ -1,9 +1,9 @@
 function Sholo() {
 }
 
-Sholo.prototype.coolMethod3 = function(arg0, success, error) {
-  console.log("en coolMethod3 1145", arg0);
-  cordova.exec(success, error, "Sholo", "coolMethod2", [arg0]);
+Sholo.prototype.coolMethod3 = function(arg0, successCallback, errorCallback) {
+  console.log("en coolMethod3 1153", arg0);
+  cordova.exec(successCallback, errorCallback, "Sholo", "coolMethod2", [arg0]);
 };
 
 Sholo.install = function () {
